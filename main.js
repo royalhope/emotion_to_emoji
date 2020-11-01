1f98928878bbe4f4c283db12fba5e39097e24136
royalhope <70911264+royalhope@users.noreply.github.com>
2020-11-01 15:49:10 +0530

    Finished!

--- main.js
+++ main.js
@@ -22,5 +22,52 @@ function modelLoaded() {
     console.log('Model Loaded!');
 }
 function check() {
-    
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
 }
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+
+        console.log(results[0].label);
+        console.log(results[1].label);
+
+        if(results[0].label == "Happy") {
+            document.getElementById("update_emoji").innerHTML = "&#128522;";
+        }
+
+        if(results[0].label == "Sad") {
+            document.getElementById("update_emoji").innerHTML = "&#128532;";
+        }
+
+        if(results[0].label == "Angry") {
+            document.getElementById("update_emoji").innerHTML = "&#128548;";
+        }
+
+        if(results[0].label == "Shocked") {
+            document.getElementById("update_emoji").innerHTML = "&#x1f615;";
+        }
+
+        if(results[1].label == "Happy") {
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";
+        }
+
+        if(results[1].label == "Angry") {
+            document.getElementById("update_emoji2").innerHTML = "&#128532;";
+        }
+
+        if(results[1].label == "Sad") {
+            document.getElementById("update_emoji2").innerHTML = "&#128548;";
+        }
+
+        if(results[1].label == "Shocked") {
+            document.getElementById("update_emoji2").innerHTML = "&#x1f615;";
+        }
+    }
+};
